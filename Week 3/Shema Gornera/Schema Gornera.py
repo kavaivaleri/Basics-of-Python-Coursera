0d232d2c84e9945bc9b8d505f8dc8e9bbb46ac0d
kavaivaleri <71337024+kavaivaleri@users.noreply.github.com>
2021-06-22 13:12:00 +0300

    Update Schema Gornera.py

--- Week 3/Shema Gornera/Schema Gornera.py
+++ Week 3/Shema Gornera/Schema Gornera.py
@@ -1,1 +1,17 @@
+n = int(input())
+x = float(input())
 
+i = 1
+list_koeff = []
+
+for i in range(1, n + 2):
+    list_koeff.append(float(input()))
+
+list_x = [x]
+itog_list = [list_koeff[-1]]
+
+for i in range(2, n + 2):
+    itog_list.append(itog_list[-1] + list_koeff[-i] * list_x[-1])
+    list_x.append(list_x[-1] * x)
+
+print(itog_list[-1])
